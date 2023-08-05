a8539744c325eee5dd436e0a5075f06ab5bdf400
John Stanley Altonaga <johntaltonaga@gmail.com>
2023-08-05 23:25:57 +0800

    Add id for links

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,4 +1,6 @@
 import SkillTag from "@/components/Keyword";
+import Link from "next/link";
+import { AiOutlineDown } from "react-icons/ai";
 
 export default function Hero() {
   const skills = [
@@ -15,7 +17,7 @@ export default function Hero() {
   ];
 
   return (
-    <section className="flex mb-24 lg:mb-0 lg:min-h-screen flex-col items-center pt-24">
+    <section className="mb-24 lg:mb-0 lg:min-h-screen flex flex-col items-center pt-24">
       <h5 className="text-white text-3xl">Stanley Altonaga</h5>
       <p className="text-purple-500 text-xl">Full-stack Web Developer</p>
       <div className="mt-12 flex flex-row gap-3 items-center justify-center flex-wrap max-w-lg md:max-w-xl ">
@@ -26,12 +28,16 @@ export default function Hero() {
         ))}
       </div>
       <div className="mt-10 flex gap-4 flex-wrap justify-center max-w-sm md:max-w-xl">
-        <button className="py-2 px-16 w-fit font-medium bg-purple-500 hover:bg-purple-400 shadow-purple text-midnight ">
-          contact me
-        </button>
-        <button className="py-2 px-16 font-medium border border-white text-white hover:bg-white hover:text-midnight">
-          works
-        </button>
+        <Link href="#contact">
+          <button className="py-2 px-16 w-fit font-medium bg-purple-500 hover:bg-purple-400 shadow-purple text-midnight ">
+            contact me
+          </button>
+        </Link>
+        <Link href="#works">
+          <button className="flex items-center gap-2 py-2 px-16 font-medium border border-white text-white hover:bg-white hover:text-midnight">
+            works <AiOutlineDown />
+          </button>
+        </Link>
       </div>
     </section>
   );
--- components/Works.jsx
+++ components/Works.jsx
@@ -3,7 +3,10 @@ import WorksData from "../data/works.js";
 
 export default function Works() {
   return (
-    <section className="min-h-screen flex flex-col items-center">
+    <section
+      className="mb-24 lg:mb-12 lg:min-h-screen flex flex-col items-center"
+      id="works"
+    >
       <h1 className="text-white text-6xl mb-12  ">My Works</h1>
       <div className="h-100 w-100 md:h-1/3 md:w-2/3">
         <CustomCarousel images={WorksData} />
