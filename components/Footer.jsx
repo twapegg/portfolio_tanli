f459271f13172dc544999865c9c422d88e125e89
John Stanley Altonaga <johntaltonaga@gmail.com>
2023-08-05 23:23:24 +0800

    Add footer section

---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="flex flex-col items-center justify-center h-24 text-white">
+        <p className="text-sm">Built by John Stanley Altonaga</p>
+        <p className="text-sm">All Rights Reserved. ©</p>
+      </div>
+    </footer>
+  );
+}
